e9a2dac15db09071cdd1665aa27b0898015873e1
Coi-Zhi <coi597262137@gmail.com>
2026-03-06 14:21:59 +0100

    Revise run.bat for adb setup and webcam launch
    Updated run.bat to set up adb forwards and start webcam receiver.

--- run.bat
+++ run.bat
@@ -1,2 +1,13 @@
-cd /d PWebCam
-start_cam.bat
+@echo off
+cd /d %~dp0
+
+echo Removing old adb forwards...
+.\adb.exe forward --remove-all
+
+echo Creating adb forward tcp:9000 -> tcp:9000 ...
+.\adb.exe forward tcp:9000 tcp:9000
+
+echo Starting webcam receiver...
+.\webcam.exe
+
+pause
